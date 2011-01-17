af31c2e3f7ba5a96778f924405608686d71fe130
ssm2017 <ssm2017.binder@gmail.com>
2011-01-17 19:55:45 +0100

    added : copyrignt notice

--- societe/DolWsSociete.php
+++ societe/DolWsSociete.php
@@ -1,4 +1,13 @@
 <?php
+/**
+ * @package DolWs
+ * @copyright Copyright (C) 2011 Wene / ssm2017 Binder (S.Massiaux). All rights reserved.
+ * @license   GNU/GPL, http://www.gnu.org/licenses/gpl-2.0.html
+ * DolWs is free software. This version may have been modified pursuant to the GNU General Public License,
+ * and as distributed it includes or is derivative of works licensed under the GNU General Public License
+ * or other free or open source software licenses.
+ */
+ 
 global $conf, $langs;
 require_once(DOL_DOCUMENT_ROOT."/lib/company.lib.php");
 require_once(DOL_DOCUMENT_ROOT."/contact/class/contact.class.php");
--- societe/server.php
+++ societe/server.php
@@ -1,5 +1,13 @@
 <?php
-
+/**
+ * @package DolWs
+ * @copyright Copyright (C) 2011 Wene / ssm2017 Binder (S.Massiaux). All rights reserved.
+ * @license   GNU/GPL, http://www.gnu.org/licenses/gpl-2.0.html
+ * DolWs is free software. This version may have been modified pursuant to the GNU General Public License,
+ * and as distributed it includes or is derivative of works licensed under the GNU General Public License
+ * or other free or open source software licenses.
+ */
+ 
 require_once("../../../master.inc.php");
 require_once(DOL_DOCUMENT_ROOT."/lib/functions2.lib.php");
 require_once(NUSOAP_PATH.'/nusoap.php');    // Include SOAP
